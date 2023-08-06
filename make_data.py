dc41abc1e8e9a95491baf654050ffc07731fda3a
MarcusYearwood <mxyearwood@gmail.com>
2023-08-06 18:18:39 -0400

    more data

---
+++ make_data.py
@@ -0,0 +1,6 @@
+# delete text inside of parenthases
+# split data by sentence
+# split data by new line
+
+# make corresponding data for each data point
+# ask chatgpt for alternating styles (simple, funny, bullet points, elif, )
